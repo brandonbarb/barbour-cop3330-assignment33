8966e77323d8689d4f3c5aaf574426d8e73ac6b0
Brandon Barbour <brandon8barbour@gmail.com>
2021-06-13 23:22:04 -0400

    Initial commit

---
+++ src/main/java/ex33.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 33 Solution
+ *  Copyright 2021 Brandon Barbour
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ex33 {
+    public static void main(String[] args) {
+        String[] answers = {"Yes", "No", "Maybe", "Ask again later."};
+        Random number = new Random();
+        System.out.print("What's your question?\n> ");
+        Scanner input = new Scanner(System.in);
+        String question = input.next();
+
+        int i = number.nextInt(4);
+        System.out.printf("%s", answers[i]);
+    }
+}
+
